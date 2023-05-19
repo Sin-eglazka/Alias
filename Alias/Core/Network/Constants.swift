bdbf4bd2dbd786f283f747ea4785297410738984
Sin-eglazka <k.akst@mail.ru>
2023-05-19 16:26:14 +0300

    Add modules

---
+++ Alias/Core/Network/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Alias
+//
+//  Created by Kate on 19.05.2023.
+//
+
+import Foundation
+
+enum Constants{
+    static let baseUrl = "https://alias-swift-api.onrender.com/"
+}
+
+enum Endpoints{
+    static let users = "users"
+    static let login = "users/login"
+    static let register = "users/register"
+}
--- Alias/Core/Network/HttpClient.swift
+++ Alias/Core/Network/HttpClient.swift
@@ -13,11 +13,25 @@ class HttpClient{
     
     static let shared = HttpClient()
     
-    static let baseUrl = "https://alias-swift-api.onrender.com/"
+
     
+    func fetch <T: Codable> (url: URL) async throws -> [T]{
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else{
+            throw HttpError.badResponse
+        }
+        
+        guard let object = 	try? JSONDecoder().decode([T].self, from: data) else{
+            throw HttpError.errorDecodingData
+        }
+        return object
+    }
     
 }
 
-enum Endpoints{
-    static let users = "users"
+enum HttpError: Error{
+    case badURL, badResponse, errorDecodingData, invalidURL
 }
+
+
